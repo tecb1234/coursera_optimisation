ab164c2e8fde49c959494988add4558c61fdf028
t_baynes <t_baynes@hotmail.com>
2016-11-19 22:49:41 +0000

    added greedy algorithms based on value, weight and value/weight

--- greedy_knapsack.py
+++ greedy_knapsack.py
@@ -1,28 +1,62 @@
 
+from operator import attrgetter
+from collections import namedtuple
 
-def greedy_knapsack(item_count: int, capacity: int, items: object, approach: str) -> object:
+def greedy_knapsack(item_count: int, capacity: int, items: list, approach: str) -> object:
     # a function to apply a simple greedy algorithm to the Knapsack problem
     #
 
     """
 
-    :rtype: int
+    :rtype: str
     """
     if approach == "value":
         # choose the most valuable items first
         # sort items by value
 
-        x = 1
+        sorted_items = sorted(items, key = attrgetter('value'), reverse = True)
+
+        x = items
 
     elif approach == "smallest":
         # choose the smallest items first
-        x = 2
+
+        sorted_items = sorted(items, key = attrgetter('weight'))
+        x = sorted_items
 
     elif approach == "value_to_weight":
         # choose the items with best value to weight ratio first
-        x = 3
+
+        item1 = namedtuple("Item", ['index', 'value', 'weight', 'value_to_weight'])
+        for item in items:
+            value_to_weight = item.value / item.weight
+            items[item.index] = item1(item.index, item.value, item.weight, value_to_weight)
+
+        sorted_items = sorted(items, key = attrgetter('value_to_weight'), reverse = True)
+
+
+        x = items[3]
     else:
         # something has gone wrong
-        x = 4
+        x = items[4]
 
-    return x
+    # now do something with the sorted_items
+    value = 0
+    weight = 0
+    taken = [0]*len(items)
+    sorted_taken = [0]*len(items)
+
+    for item in sorted_items:
+        if weight + item.weight <= capacity:
+            taken[item.index] = 1
+            weight += item.weight
+            value += item.value
+
+    # for index in sorted_items:
+    #     taken[item.index] = sorted_taken[index]
+
+    # make the output
+    output_data = str(value) + ' ' + str(0) + '\n'
+    output_data += ' '.join(map(str, taken))
+
+    return output_data
