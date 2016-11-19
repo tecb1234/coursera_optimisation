f7ec2da9caa75c5f584a7e8c3fb425f390ae8c51
t_baynes <t_baynes@hotmail.com>
2016-11-19 21:14:40 +0000

    discovered elif

--- greedy_knapsack.py
+++ greedy_knapsack.py
@@ -1,5 +1,6 @@
 print('blah')
 
+
 def greedy_knapsack(item_count: int, capacity: int, items: object, approach: str) -> object:
     # a function to apply a simple greedy algorithm to the Knapsack problem
     #
@@ -14,20 +15,15 @@ def greedy_knapsack(item_count: int, capacity: int, items: object, approach: str
 
         x = 1
 
+    elif approach == "smallest":
+        # choose the smallest items first
+        x = 2
 
+    elif approach == "value_to_weight":
+        # choose the items with best value to weight ratio first
+        x = 3
     else:
-        if approach == "smallest":
-            # choose the smallest items first
-            x = 2
+        # something has gone wrong
+        x = 4
 
-        else:
-            if approach == "value_to_weight":
-                # choose the items with best value to weight ratio first
-                x = 3
-            else:
-                # something has gone wrong
-                x = 4
     return x
-
-
-
